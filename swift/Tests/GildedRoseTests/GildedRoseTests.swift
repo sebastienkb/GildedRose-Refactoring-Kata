6eea9d1a28b3d58b6ba8e78879929ea4ab965857
Sébastien Kalb <sebastien@mytribunews.com>
2022-01-30 11:45:28 +0100

    Add test case for sellIn decreases

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -10,6 +10,20 @@ class GildedRoseTests: XCTestCase {
         XCTAssertEqual(app.items[0].name, "foo")
     }
 
+    func testSellInDecreasesForNonLegendaryItems() {
+        let items = [
+            Item(name: "Potato", sellIn: 2, quality: 0),
+            Item(name: "Aged Brie", sellIn: 1, quality: 0),
+            Item(name: "Elixir of the Mongoose", sellIn: 0, quality: 0),
+            Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: -1, quality: 0)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        XCTAssertEqual(app.items[0].sellIn, 1)
+        XCTAssertEqual(app.items[1].sellIn, 0)
+        XCTAssertEqual(app.items[2].sellIn, -1)
+        XCTAssertEqual(app.items[3].sellIn, -2)
+    }
+
     func testQualityDegradesTwiceAsFastOnceTheSellDateHasPassed() {
         let originalQuality = 10
         let items = [Item(name: "Potato", sellIn: -1, quality: originalQuality)]
