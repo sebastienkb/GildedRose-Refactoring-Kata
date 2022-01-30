f1b33aeb9a164c5e1204c6cd0ef366ea21df76fd
Sébastien Kalb <sebastien@mytribunews.com>
2022-01-30 10:28:32 +0100

    Tuck away quality logic behind QualityEvolution enum

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -61,6 +61,42 @@ public class GildedRose {
 private extension Item {
 
     var qualityEvolution: QualityEvolution { QualityEvolution(for: name) }
+
+    func updateQuality() {
+        switch qualityEvolution {
+
+        case .decreasing:
+            sellIn = sellIn - 1
+            let multiplier = sellIn < 0 ? 2 : 1
+            quality = max(Constants.minimumQuality, quality - 1 * multiplier)
+
+        case .increasing:
+            sellIn = sellIn - 1
+            quality = min(Constants.maximumRegularQuality, quality + 1)
+
+        case .pumpAndDump:
+            sellIn = sellIn - 1
+            switch sellIn {
+            case ..<(-1):
+                quality = Constants.minimumQuality
+            case 0...5:
+                quality = min(Constants.maximumRegularQuality, quality + 3)
+            case 6...10:
+                quality = min(Constants.maximumRegularQuality, quality + 2)
+            default:
+                quality = min(Constants.maximumRegularQuality, quality + 1)
+            }
+
+        case .unaffected:
+            quality = Constants.maximumLegendaryQuality // `break` would also be valid because its init contains 80; this would be a post-init corrective measure
+        }
+    }
+
+    enum Constants {
+        static let minimumQuality = 0
+        static let maximumRegularQuality = 50
+        static let maximumLegendaryQuality = 80
+    }
 }
 
 private enum QualityEvolution {
